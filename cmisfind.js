d117f8f4e2e2206bb5283d43982a7eb4650b9a6b
Trenton D. Adams <trent@trentonadams.ca>
2016-10-22 14:51:49 -0600

    - refactor to better form.  We don't need to do queries for the folder, we   just get it by path.

--- cmisfind.js
+++ cmisfind.js
@@ -13,7 +13,6 @@ var logLevel = 'info';
 var cmisUser = require("./cmis-user.json");
 var cmis = require('cmis');
 var maxdepth = -1;
-var folderName = '';
 var folderPath = '';
 var name = '';
 var attributes = "cmis:name, cmis:objectId";
@@ -21,93 +20,79 @@ var attributes = "cmis:name, cmis:objectId";
 console.log(cmisUser);
 function help()
 {
-    console.log('Incorrect usage:');
-    console.log(process.argv[1] +
-        ' /Sites/office-of-the-registrar/documentLibrary/' +
-        'EnterpriseFolderDoesntExistAPAS [-maxdepth #] ' +
-        '[-name SQL-like-query] [-loglevel info,error,debug] ');
+  console.log('Incorrect usage:');
+  console.log(process.argv[1] +
+      ' /Sites/site-name/documentLibrary/' +
+      ' [-maxdepth #] [-name SQL-like-query] [-loglevel info,error,debug] ');
 }
 
-process.argv.forEach(function (val, index, array) {
-    if (index == 2 && array[index] !== undefined)
+process.argv.forEach(function (val, index, array)
+{
+  if (index == 2 && array[index] !== undefined)
+  {
+    folderPath = array[index];
+    winston.log('searching name: , path: ' + folderPath);
+  }
+  else if (folderPath !== undefined)
+  {
+    switch (val)
     {
-        folderPath = array[index];
-        folderName = folderPath.replace(/.*\/(.*)/, '$1');
-        winston.log('searching name: , path: ' + folderPath);
+      case '-maxdepth':
+        maxdepth = array[index + 1];
+        break;
+      case '-name':
+        name = array[index + 1];
+        break;
+      case '-loglevel':
+        logLevel = array[index + 1];
+        break;
     }
-    else if (folderPath !== undefined)
-    {
-        switch (val)
-        {
-            case '-maxdepth':
-                maxdepth = array[index + 1];
-                break;
-            case '-name':
-                name = array[index + 1];
-                break;
-            case '-loglevel':
-                logLevel = array[index + 1];
-                break;
-        }
-    }
+  }
 });
 
-if (folderPath === '' || folderName === '')
+if (folderPath === '')
 {
-    help();
-    process.exit();
+  help();
+  process.exit();
 }
-winston.log('info', 'searching name: ' + folderName + ', path: ' + folderPath);
+winston.log('info', 'searching path: ' + folderPath);
 
-winston.add(winston.transports.File, { filename: './cmisfind.log',
-    level: logLevel});
+winston.add(winston.transports.File, {
+  filename: './cmisfind.log',
+  level: logLevel
+});
 winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, { level: 'error' });
+winston.add(winston.transports.Console, {level: 'error'});
 
 
 var session = cmis.createSession(cmisUser.url);
 session.setCredentials(cmisUser.username, cmisUser.password);
 var repositories = session.loadRepositories().ok(function (data)
 {
-    winston.log('debug', '{data:', data, '}');
-    session.query("" +
-        " SELECT * " +
-        "   FROM cmis:folder " +
-        "  WHERE cmis:name = '" + folderName + "'",
-        false).ok(function (data)
+  winston.log('debug', '{data:', data, '}');
+  // get the actual folder being searched
+  session.getObjectByPath(folderPath).ok(function (data) {
+    var likeQuery = name != '' ?
+        ("cmis:name LIKE '" + name + "'\n    AND ") : '';
+    var query = "" +
+        " SELECT " + attributes + "\n" +
+        "   FROM cmis:document \n" +
+        "  WHERE " + likeQuery +
+        "        IN_TREE('" + data.succinctProperties['cmis:objectId'] +
+        "') \n";
+    winston.log('debug', query);
+    session.query(query, false).ok(function (data)
     {
-        var auStudents = {'cmis:objectId': undefined};
-        winston.log('debug', data);
-        for (i = 0; i< data.results.length; i++)
-        {
-            winston.log('debug', "au-students: ", data.results[i]);
-            if (data.results[0].succinctProperties['cmis:path'] == folderPath)
-            {
-                auStudents = data.results[0].succinctProperties;
-                winston.log('info', auStudents['cmis:objectId']);
-                var likeQuery = name != '' ?
-                    ("cmis:name LIKE '" + name + "'\n    AND ") : '';
-                var query = "" +
-                    " SELECT " + attributes +"\n" +
-                    "   FROM cmis:document \n" +
-                    "  WHERE " + likeQuery +
-                    "        IN_TREE('" + auStudents['cmis:objectId'] + "') \n";
-                winston.log('debug', query);
-                session.query(query, false).ok(function (data)
-                {
-                    for (i = 0; i< data.results.length; i++)
-                    {
-                        winston.log('debug', data.results[i]);
-                        console.log(data.results[i].succinctProperties['cmis:objectId']);
-                    }
-                }).notOk(function(data) {
-                    console.log('error: ', data);
-                });
-            }
-        }
-    }).notOk(function(data){
-        console.log('error: ', data);
+      for (i = 0; i < data.results.length; i++)
+      {
+        winston.log('debug', data.results[i]);
+        console.log(data.results[i].succinctProperties['cmis:objectId']);
+      }
+    }).notOk(function (data)
+    {
+      console.log('error: ', data);
     });
+  });
 });
 
 
