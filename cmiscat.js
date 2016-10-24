377241a3f3a82ca7d6563b0570f7824ce630cbc8
Trenton D. Adams <trent@trentonadams.ca>
2016-10-24 16:49:04 -0600

    - do proper binary downloading using cmis and request modules

--- cmiscat.js
+++ cmiscat.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+"use strict";
 /*
  Also try...
  * var Promise = require("bluebird");
@@ -7,46 +7,65 @@
  */
 // cmis-user.json format: {"username": "user", "password": "9cz0iiasf90ui"}
 
+var cmis = require('cmis');
+var winston = require('winston');
+var url = require('url');
+var fs = require('fs');
 var cmisUser = require("./cmis-user.json");
 var objectId = '';
+var logLevel = 'info';
+var request = require('request');
+
+winston.add(winston.transports.File, {
+  filename: './file-utils.log',
+  level: logLevel
+});
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {level: 'error'});
+
 
 function help()
 {
-    console.log('Incorrect usage:');
-    console.log('Accepts direct input from cmisfind.js through a unix pipe');
-    console.log(process.argv[1] +
-        ' workspace://SpacesStore/a32d1179-3663-478f-a489-8ad82602accf;1.0');
+  console.log('Incorrect usage:');
+  console.log('Accepts direct input from cmisfind.js through a unix pipe');
+  console.log(process.argv[1] +
+      ' workspace://SpacesStore/a32d1179-3663-478f-a489-8ad82602accf;1.0');
 }
 
 process.argv.forEach(function (val, index, array)
 {
-    if (index == 2 && array[index] !== undefined)
-    {
-        objectId = array[index];
-        objectId = objectId.replace(/.*\/(.*);.*/, '$1');
-    }
+  if (index == 2 && array[index] !== undefined)
+  {
+    objectId = array[index];
+    objectId = objectId.replace(/.*\/(.*);.*/, '$1');
+  }
 });
 
 if (objectId === '')
 {
-    help();
-    process.exit();
+  help();
+  process.exit();
 }
 
-var Client = require('node-rest-client').Client;
+var session = cmis.createSession(cmisUser.url);
+session.setCredentials(cmisUser.username, cmisUser.password);
+session.loadRepositories().ok(function (data)
+{
+  winston.log('debug', '{data: ', data, '}');
+  // get the actual folder being searched
+  var contentUrl = session.getContentStreamURL(objectId, true);
 
-var client = new Client({
-    user: cmisUser.username,
-    password: cmisUser.password
-});
-
-// registering remote methods
-client.registerMethod("jsonMethod",
-    "https://banalf-test.athabascau.ca/alfresco/api/-default-/public/cmis/versions/1.1/atom/content?id=" +
-    objectId, "GET");
-
-client.methods.jsonMethod(function (data, response) {
-    console.log(data.toString('binary'));
-    // raw response
-//    DEBUG console.log(response);
+  request(contentUrl, {
+    encoding: null, 'auth': {
+      'user': cmisUser.username,
+      'pass': cmisUser.password,
+      'sendImmediately': false
+    }
+  }, function (error, response, body)
+  {
+    process.stdout.write(body);
+  });
+}).notOk(function (data)
+{
+  winston.log('error', 'failed to retrieve repository information: ', data);
 });
