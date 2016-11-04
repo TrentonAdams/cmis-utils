f6f499c6097bd1788fe17babf26a69065ec8a44a
Trenton D. Adams <trenta@athabascau.ca>
2016-11-04 17:25:45 -0600

    - add a verbose flag and log removed files to console only if it's set

--- cmisrm.js
+++ cmisrm.js
@@ -12,20 +12,33 @@ var logLevel = 'debug';
 
 var cmisUser = require("./cmis-user.json");
 var cmis = require('cmis');
+var verbose = false;
+var objectId = '';
 
 function help()
 {
     console.log('Incorrect usage:');
     console.log(process.argv[1] +
-        ' workspace://SpacesStore/a32d1179-3663-478f-a489-8ad82602accf;1.0');
+        ' [ -v ] workspace://SpacesStore/a32d1179-3663-478f-a489-8ad82602accf;1.0');
+    console.log("\t -v\t verbose: log IDs removed")
 }
 
+//console.log(process.argv);
 process.argv.forEach(function (val, index, array)
 {
-    if (index == 2 && array[index] !== undefined)
+    if (index == (array.length - 1) && array.length > 2)
     {
         objectId = array[index];
         objectId = objectId.replace(/.*\/(.*);.*/, '$1');
+    }
+    else
+    {
+        switch (val)
+        {
+            case '-v':
+                verbose = true;
+                break;
+        }
     }
 });
 
@@ -40,15 +53,19 @@ winston.add(winston.transports.File, { filename: './cmis-utils.log',
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, { level: 'error' });
 
+//console.log('objectId: ', objectId);
+
 
 var session = cmis.createSession(cmisUser.url);
 session.setCredentials(cmisUser.username, cmisUser.password);
 var repositories = session.loadRepositories().ok(function (data)
 {
     winston.log('debug', '{data:', data, '}');
-    session.deleteObject(objectId, true).ok(function (data)
-    {
-        console.log("remove: " + objectId);
+    session.deleteObject(objectId, true).ok(function (data) {
+        if (verbose)
+        {
+            console.log("removed '" + objectId + "'");
+        }
     }).notOk(function(data){
         console.log('error: ', data);
     });
